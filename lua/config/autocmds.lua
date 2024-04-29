175506cef2927545a433d942483d5586c0bb780f
Anton Ödman <anton.odman@gmail.com>
2024-04-29 10:55:50 +0200

    chore: remove comments on new line

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,13 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Disable auto-comment for new lines
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = function()
+    -- Remove 'o' and 'r' from formatoptions for all file types
+    vim.opt.formatoptions:remove("o")
+    vim.opt.formatoptions:remove("r")
+  end,
+})
