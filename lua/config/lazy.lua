310ae20647629db6d4bf5e7ec6d2185b9f44bccf
Anton Ödman <anton.odman@gmail.com>
2024-04-30 10:07:11 +0200

    chore: add tests

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,6 +23,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.util.dot" },
     { import = "lazyvim.plugins.extras.vscode" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.test.core" },
+    { import = "lazyvim.plugins.extras.dap.core" },
     -- import/override with your plugins
     { import = "plugins" },
   },
