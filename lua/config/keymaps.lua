eb5e89aaf24d567ef462c03afb89e88366c3405c
Anton Ödman <anton.odman@gmail.com>
2024-05-02 14:58:38 +0200

    chore: add keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- "n", "<c-/>", lazyterm, { desc = "Terminal (Root Dir)" }
+
+local lazyterm = function()
+  LazyVim.terminal(nil, { cwd = LazyVim.root() })
+end
+
+vim.keymap.set("n", "<leader>ö", lazyterm, { desc = "Terminal (Root Dir)" })
+vim.keymap.set("n", "<C-ö>", lazyterm, { desc = "Terminal (Root Dir)" }) -- TODO: why doesn't this work?
