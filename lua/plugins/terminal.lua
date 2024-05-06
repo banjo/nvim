cd1c47cf79ca9d622243953b93474f9992aef3ec
Anton Ödman <anton.odman@gmail.com>
2024-05-06 15:22:45 +0200

    chore: update terminal

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,9 +1,4 @@
 return {
   "akinsho/toggleterm.nvim",
-  opts = {
-    open_mapping = [[<leader>ö]],
-    insert_mappings = true,
-    shade_terminals = false,
-    direction = "horizontal",
-  },
+  enabled = false,
 }
