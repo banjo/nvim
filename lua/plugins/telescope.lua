16cc68c26c567a399ced6ec8c92b820771eb2e52
Anton Ödman <anton.odman@gmail.com>
2024-05-06 14:43:04 +0200

    chore: add current buffer search

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -31,6 +31,26 @@ return {
         end,
         desc = "Find recent files",
       },
+      {
+        "<leader>/",
+        function()
+          require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+            winblend = 10,
+            previewer = false,
+          }))
+        end,
+        desc = "Search in current buffer",
+      },
+      {
+        "<leader>s/",
+        function()
+          require("telescope.builtin").live_grep({
+            grep_open_files = true,
+            prompt_title = "Live Grep in Open Files",
+          })
+        end,
+        desc = "Live grep in open files",
+      },
     },
     config = function(_, opts)
       require("telescope").load_extension("undo")
