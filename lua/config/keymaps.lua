85824528a73597d10cb442a204b31a6390145e00
Anton Ödman <anton.odman@gmail.com>
2024-05-08 15:20:07 +0200

    chore: add spectre shortcut

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,6 +3,7 @@
 -- Add any additional keymaps here
 -- "n", "<c-/>", lazyterm, { desc = "Terminal (Root Dir)" }
 
+-- Terminal
 local lazyterm = function()
   LazyVim.terminal(nil, { cwd = LazyVim.root() })
 end
@@ -10,4 +11,10 @@ end
 vim.keymap.set("n", "<leader>ö", lazyterm, { desc = "Terminal (Root Dir)" })
 vim.keymap.set("n", "<C-ö>", lazyterm, { desc = "Terminal (Root Dir)" }) -- TODO: why doesn't this work?
 
+-- ChatGPT
 vim.keymap.set("n", "<leader>gp", "<cmd>ChatGPT<CR>", { desc = "ChatGPT" })
+
+-- Spectre
+vim.keymap.set("n", "<leader>so", function()
+  require("spectre").open_file_search({ select_word = true })
+end, { desc = "Search in open files (Spectre)" })
