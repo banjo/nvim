3356fc0781cb833d8b6604dc2035654cddeee1db
Anton Ödman <anton.odman@gmail.com>
2024-05-19 19:17:28 +0100

    chore: try tmux navigation and update docs

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -1,4 +1,19 @@
 return {
-  -- "christoomey/vim-tmux-navigator",
-  -- enabled = false,
+  "christoomey/vim-tmux-navigator",
+  enabled = false,
+  -- event = "BufReadPre",
+  -- cmd = {
+  --   "TmuxNavigateLeft",
+  --   "TmuxNavigateDown",
+  --   "TmuxNavigateUp",
+  --   "TmuxNavigateRight",
+  --   "TmuxNavigatePrevious",
+  -- },
+  -- keys = {
+  --   { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+  --   { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+  --   { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+  --   { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+  --   { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+  -- },
 }
