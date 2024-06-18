38b944f1f6ac4b86367dcd7a02a2f7681e74e4d7
Anton Ödman <anton.odman@gmail.com>
2024-06-18 18:32:31 +0200

    chore: change close cmp to ctrl c

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -34,12 +34,6 @@ return {
   "hrsh7th/nvim-cmp",
   ---@param opts cmp.ConfigSchema
   opts = function(_, opts)
-    local has_words_before = function()
-      unpack = unpack or table.unpack
-      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-    end
-
     local luasnip = require("luasnip")
     local cmp = require("cmp")
 
@@ -56,7 +50,7 @@ return {
       }),
       -- Explicitly request completions.
       ["<C-Space>"] = cmp.mapping.complete(),
-      ["/"] = cmp.mapping.close(),
+      ["<C-c>"] = cmp.mapping.close(),
       -- Overload tab to accept Copilot suggestions.
       ["<Tab>"] = cmp.mapping(function(fallback)
         local copilot = require("copilot.suggestion")
