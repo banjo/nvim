2574c511d4003c27155585003e3d6e8e3ecd75c8
Anton Ödman <anton.odman@gmail.com>
2024-06-18 11:35:41 +0200

    chore: add different color to dracula

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,16 @@
 return {
-  { "Mofiqul/dracula.nvim" },
+  {
+    "Mofiqul/dracula.nvim",
+    config = function()
+      require("dracula").setup({
+        overrides = function(colors)
+          return {
+            NormalFloat = { fg = colors.fg, bg = colors.menu },
+          }
+        end,
+      })
+    end,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
