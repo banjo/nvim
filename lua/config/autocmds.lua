60d8b35bbbd815297d6fcb5beb3b722c00e70398
Anton Ödman <anton.odman@gmail.com>
2024-06-22 22:17:55 +0200

    chore: no diagnostics for .env

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -39,3 +39,13 @@ vim.api.nvim_create_autocmd("FileType", {
     })
   end,
 })
+
+-- Disable diagnostics for .env files
+local group = vim.api.nvim_create_augroup("__env", { clear = true })
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = ".env",
+  group = group,
+  callback = function(args)
+    vim.diagnostic.disable(args.buf)
+  end,
+})
