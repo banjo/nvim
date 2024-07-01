f38e55ffe3436a644942ca6a80b33cecda651ff1
Anton Ödman <anton.odman@gmail.com>
2024-07-01 19:54:23 +0200

    chore: better markdown preview

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,7 +18,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.git" },
-    { import = "lazyvim.plugins.extras.lang.markdown" },
+    -- { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.astro" },
     { import = "lazyvim.plugins.extras.lang.docker" },
