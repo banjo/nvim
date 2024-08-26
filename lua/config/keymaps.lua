307ecf8925b9d658d8b6f2e61d0987bbd462afda
Anton Ödman <anton.odman@gmail.com>
2024-08-26 18:35:53 +0200

    chore: add keymap for json

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,6 +9,9 @@ vim.keymap.set("n", "<leader>fn", "<cmd>enew<CR>", { desc = "[f]ile [n]ew" })
 vim.keymap.set("n", "<leader>ft", "<Nop>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>fT", "<Nop>", { noremap = true, silent = true })
 
+-- set filetype=json
+vim.keymap.set("n", "<leader>ftj", "<cmd>set ft=json<CR>", { desc = "[f]ile [t]ype [j]son" })
+
 -- Remove one character without yanking
 vim.keymap.set("n", "x", '"x')
 
