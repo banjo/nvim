3fe0b4ec3d00b510c5be01bdfe7404734d9fa8ad
Anton Ödman <anton.odman@gmail.com>
2024-09-13 19:50:33 +0200

    chore: remove swapfile

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Sync buffers automatically
+vim.opt.autoread = true
+
+-- disable swap swapfile and showing error
+vim.opt.swapfile = false
