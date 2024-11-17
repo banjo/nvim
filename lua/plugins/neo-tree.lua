31db69076d5546789167a89257c4cb39af632f08
Anton Ödman <anton.odman@gmail.com>
2024-11-17 22:21:30 +0100

    chore: neo tree on right side

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -41,7 +41,7 @@ return {
     {
       "<leader>fe",
       function()
-        require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
+        require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd(), position = "right" })
       end,
       desc = "[f]ind [e]xplorer",
     },
