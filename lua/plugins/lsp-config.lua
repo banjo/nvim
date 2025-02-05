76023d3f9d981ac27c316cd739f34dd4e18d0e85
Anton Ödman <anton.odman@gmail.com>
2025-02-05 17:17:42 +0100

    chore: config for some things

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -3,22 +3,16 @@ return {
     "neovim/nvim-lspconfig",
     ---@class PluginLspOpts
     opts = {
-      ---@type lspconfig.options
       inlay_hints = {
         enabled = false,
       },
-      servers = {
-        vtsls = {
-          settings = {
-            vtsls = { autoUseWorkspaceTsdk = true },
+      capabilities = {
+        workspace = {
+          didChangeWatchedFiles = {
+            dynamicRegistration = false,
           },
         },
       },
     },
-  },
-  {
-    "hinell/lsp-timeout.nvim",
-    dependencies = { "neovim/nvim-lspconfig" },
-    enabled = false,
   },
 }
