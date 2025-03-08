b3953245744d698d5d99b1d3906bf6fe4bbd2a6a
Anton Ödman <anton.odman@gmail.com>
2025-03-08 23:41:32 +0700

    chore: add message when waiting for reply

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -4,7 +4,11 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
+      "j-hui/fidget.nvim",
     },
+    init = function()
+      require("config.codecompanion.fidget-spinner"):init()
+    end,
     config = function()
       local codecompanion = require("codecompanion")
 
