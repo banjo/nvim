f65d34a0ea4ea58caca373658e9879a8f3d37527
Anton Ödman <anton.odman@gmail.com>
2025-03-16 18:12:24 +0700

    chore: add neotest adapters and update keys

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -7,5 +7,8 @@ return {
     { "<F10>", function() require("dap").step_over() end, desc = "Step Over" },
     { "<F11>", function() require("dap").step_into() end, desc = "Step Into" },
     { "<S-F11>", function() require("dap").step_out() end, desc = "Step Out" },
+    -- revert step over and step out
+    { "<leader>dO", function() require("dap").step_out() end, desc = "Step Out" },
+    { "<leader>do", function() require("dap").step_over() end, desc = "Step Over" },
   },
 }
