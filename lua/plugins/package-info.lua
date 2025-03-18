e0e94976056eec4806730dfef9a88de073f11425
Anton Ödman <anton.odman@gmail.com>
2025-03-18 15:24:00 +0100

    chore: install package info

---
+++ lua/plugins/package-info.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "vuki656/package-info.nvim",
+    ft = "json",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    config = function()
+      require("package-info").setup({
+        autostart = true,
+        package_manager = "pnpm",
+        hide_up_to_date = true,
+      })
+    end,
+  },
+}
