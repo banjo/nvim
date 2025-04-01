c577af9c20739923a93f7ce19414958df49166b8
Anton Ödman <anton.odman@gmail.com>
2025-04-01 15:33:39 +0200

    chore: disable hover and implement timestamp preview

--- lua/plugins/dev.lua
+++ lua/plugins/dev.lua
@@ -1,4 +1,9 @@
 return {
   { "banjo/contextfiles.nvim", dev = true },
   { "banjo/package-pilot.nvim", dev = true },
+  {
+    "banjo/timestamp-preview.nvim",
+    dev = true,
+    opts = {},
+  },
 }
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,41 +1,46 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
-    opts = {
-      inlay_hints = {
-        enabled = false,
-      },
-      servers = {
-        vtsls = {
-          settings = {
-            typescript = {
-              preferences = {
-                includeCompletionsForModuleExports = true,
-                includeCompletionsForImportStatements = true,
-                importModuleSpecifier = "non-relative",
-                -- autoImportFileExcludePatterns = {
-                --   "dist/*",
-                --   "@radix-ui/*",
-                --   "lucide-react",
-                -- },
-              },
+    opts = function(_, opts)
+      -- disable default K, and use timestamp preview instead
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      keys[#keys + 1] = { "K", false }
+
+      -- Set inlay hints
+      opts.inlay_hints = opts.inlay_hints or {}
+      opts.inlay_hints.enabled = false
+
+      -- Configure servers
+      opts.servers = opts.servers or {}
+      opts.servers.vtsls = {
+        settings = {
+          typescript = {
+            preferences = {
+              includeCompletionsForModuleExports = true,
+              includeCompletionsForImportStatements = true,
+              importModuleSpecifier = "non-relative",
+              -- autoImportFileExcludePatterns = {
+              --   "dist/*",
+              --   "@radix-ui/*",
+              --   "lucide-react",
+              -- },
             },
           },
         },
-      },
-      setup = {
-        eslint = function()
-          -- This is needed to remove the error with eslint (sohuld work after nvim 0.11)
-          require("lazyvim.util").lsp.on_attach(function(client)
-            if client.name == "eslint" then
-              client.server_capabilities.documentFormattingProvider = true
-            elseif client.name == "tsserver" then
-              client.server_capabilities.documentFormattingProvider = false
-            end
-          end)
-        end,
-      },
-    },
+      }
+
+      -- Configure setup functions
+      opts.setup = opts.setup or {}
+      opts.setup.eslint = function()
+        -- This is needed to remove the error with eslint (should work after nvim 0.11)
+        require("lazyvim.util").lsp.on_attach(function(client)
+          if client.name == "eslint" then
+            client.server_capabilities.documentFormattingProvider = true
+          elseif client.name == "tsserver" then
+            client.server_capabilities.documentFormattingProvider = false
+          end
+        end)
+      end
+    end,
   },
 }
