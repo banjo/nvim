24bbc1e48a9068296e9a478ed360b3fe8834c009
Anton Ödman <anton.odman@gmail.com>
2025-05-19 17:01:15 +0200

    chore: fix mason

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,12 +1,17 @@
 return {
-  "williamboman/mason.nvim",
-  opts = {
-    ensure_installed = {
-      "stylua",
-      "shellcheck",
-      "shfmt",
-      "flake8",
-      "typos-lsp",
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "flake8",
+        "typos-lsp",
+      },
     },
   },
+  -- Fix for Mason breaking change: https://github.com/LazyVim/LazyVim/issues/6039
+  { "mason-org/mason.nvim", version = "1.11.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
 }
