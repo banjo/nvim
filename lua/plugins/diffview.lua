a54148a38c87af03b9fb19a871c9268a3af141a7
Anton Ödman <anton.odman@gmail.com>
2025-05-21 11:29:54 +0200

    chore: better shortcuts for diff

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -7,7 +7,7 @@ return {
     vim.keymap.set("v", "<leader>gdR", "<Esc><Cmd>'<,'>DiffviewFileHistory --follow<CR>", { desc = "range history" })
     vim.keymap.set("n", "<leader>gdl", "<Cmd>.DiffviewFileHistory --follow<CR>", { desc = "line history" })
 
-    vim.keymap.set("n", "<leader>gdr", "<cmd>DiffviewOpen<cr>", { desc = "repo diff" })
+    vim.keymap.set("n", "<leader>gdd", "<cmd>DiffviewOpen<cr>", { desc = "diff" })
 
     local function get_default_branch_name()
       local res = vim.system({ "git", "rev-parse", "--verify", "main" }, { capture_output = true }):wait()
@@ -20,7 +20,7 @@ return {
     end, { desc = "diff against origin/master" })
 
     -- Diff against develop
-    vim.keymap.set("n", "<leader>gdd", function()
+    vim.keymap.set("n", "<leader>gdD", function()
       vim.cmd("DiffviewOpen HEAD..origin/develop")
     end, { desc = "diff against develop" })
 
