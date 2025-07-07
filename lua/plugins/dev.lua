3d558eb83d9344778ad993ae9f484f7b5a334faa
Anton Ödman <anton.odman@gmail.com>
2025-07-07 09:39:08 +0200

    chore: comment out dev things

--- lua/plugins/dev.lua
+++ lua/plugins/dev.lua
@@ -1,9 +1,10 @@
-return {
-  { "banjo/contextfiles.nvim", dev = true },
-  { "banjo/package-pilot.nvim", dev = true },
-  {
-    "banjo/timestamp-preview.nvim",
-    dev = true,
-    opts = {},
-  },
-}
+return {}
+-- return {
+--   { "banjo/contextfiles.nvim", dev = true },
+--   { "banjo/package-pilot.nvim", dev = true },
+--   {
+--     "banjo/timestamp-preview.nvim",
+--     dev = true,
+--     opts = {},
+--   },
+-- }
