63bf0bc1910737a65281b2d27fa43d8c7334796c
Anton Ödman <anton.odman@gmail.com>
2025-07-13 15:12:21 +0200

    fix: test for volvo things

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -3,7 +3,13 @@ return {
     "nvim-neotest/neotest",
     dependencies = { "nvim-neotest/neotest-jest", "marilari88/neotest-vitest" },
     opts = function(_, opts)
-      table.insert(opts.adapters, require("neotest-jest"))
+      opts.log_level = vim.log.levels.INFO
+      table.insert(
+        opts.adapters,
+        require("neotest-jest")({
+          jestCommand = "npx jest --runInBand", -- default jest command without watch
+        })
+      )
       table.insert(opts.adapters, require("neotest-vitest"))
     end,
   },
