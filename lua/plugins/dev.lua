b9b6b282a4aabb80ebe8c28c455911bd092707ca
Anton Ödman <anton.odman@gmail.com>
2025-07-30 14:16:24 +0200

    chore: conditionally load dev scripts

--- lua/plugins/dev.lua
+++ lua/plugins/dev.lua
@@ -1,4 +1,18 @@
+local lazy_config = require("lazy.core.config")
+local DEV_PATH = lazy_config.options.dev.path
+
+local function should_use_dev(plugin_name)
+  local full_path = vim.fn.expand(DEV_PATH .. plugin_name)
+  return vim.fn.isdirectory(full_path) == 1
+end
+
 return {
-  { "banjo/contextfiles.nvim", dev = true },
-  { "banjo/package-pilot.nvim", dev = false },
+  {
+    "banjo/contextfiles.nvim",
+    dev = should_use_dev("contextfiles.nvim"),
+  },
+  {
+    "banjo/package-pilot.nvim",
+    dev = should_use_dev("package-pilot.nvim"),
+  },
 }
