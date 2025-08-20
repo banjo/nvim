c6664532077ff0e3aae6eab54a286be1cb57830d
Anton Ödman <anton.odman@gmail.com>
2025-08-20 15:30:47 +0200

    chore: add snippets

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,6 +1,17 @@
 -- disable tab default behavior
 return {
   "L3MON4D3/LuaSnip",
+  opts = {
+    history = true,
+    updateevents = "TextChanged,TextChangedI",
+  },
+  config = function(_, opts)
+    require("luasnip").config.set_config(opts)
+
+    require("luasnip.loaders.from_vscode").lazy_load({
+      paths = { vim.fn.stdpath("config") .. "/snippets" },
+    })
+  end,
   keys = function()
     return {}
   end,
