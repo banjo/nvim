7821d5f4e5bc40ac31d975501a6e0e31397adb57
Anton Ödman <anton.odman@gmail.com>
2025-10-01 22:00:12 +0200

    chore: update to LazyVim15

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,6 +1,6 @@
 return {
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = {
       ensure_installed = {
         "stylua",
@@ -11,7 +11,4 @@ return {
       },
     },
   },
-  -- Fix for Mason breaking change: https://github.com/LazyVim/LazyVim/issues/6039
-  { "mason-org/mason.nvim", version = "1.11.0" },
-  { "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
 }
