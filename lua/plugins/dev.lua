7ca359e78435817e9f7795a0f0de1d810627fe87
Anton Ödman <anton.odman@gmail.com>
2026-01-08 21:11:45 +0100

    chore: fix local plugin thing

--- lua/plugins/dev.lua
+++ lua/plugins/dev.lua
@@ -2,7 +2,7 @@ local lazy_config = require("lazy.core.config")
 local DEV_PATH = lazy_config.options.dev.path
 
 local function should_use_dev(plugin_name)
-  local full_path = vim.fn.expand(DEV_PATH .. plugin_name)
+  local full_path = vim.fn.expand(DEV_PATH .. "/" .. plugin_name)
   return vim.fn.isdirectory(full_path) == 1
 end
 
