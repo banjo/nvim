745e7e1555f8018f3f0e2a8b262481c390c75c0c
Anton Ödman <anton.odman@gmail.com>
2026-03-18 13:08:52 +0100

    chore: add gitlineage

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -43,4 +43,13 @@ return {
       })
     end,
   },
+  {
+    "lionyxml/gitlineage.nvim",
+    dependencies = {
+      "sindrets/diffview.nvim", -- optional, for open_diff feature
+    },
+    config = function()
+      require("gitlineage").setup()
+    end,
+  },
 }
