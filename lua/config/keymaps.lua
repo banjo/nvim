ab3ec6406d004736508468dda5b87d1caa1028c4
Anton Ödman <anton.odman@gmail.com>
2026-05-29 15:01:30 +0200

    chore: add keymaps for opencode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -376,3 +376,9 @@ vim.keymap.set("n", "<leader>j", require("treesj").toggle, { desc = "Toggle Tree
 --     apply = true,
 --   })
 -- end, { desc = "Organize Imports" })
+
+-- Terminal mode window navigation for opencode side panel
+vim.keymap.set("t", "<C-h>", "<C-\\><C-n><C-w>h", { desc = "Go to Left Window" })
+vim.keymap.set("t", "<C-j>", "<C-\\><C-n><C-w>j", { desc = "Go to Lower Window" })
+vim.keymap.set("t", "<C-k>", "<C-\\><C-n><C-w>k", { desc = "Go to Upper Window" })
+vim.keymap.set("t", "<C-l>", "<C-\\><C-n><C-w>l", { desc = "Go to Right Window" })
