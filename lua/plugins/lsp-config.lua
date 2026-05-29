573578c8798e8e7a37c271fc8c1844c1df50abd5
Anton Ödman <anton.odman@gmail.com>
2026-05-29 16:03:16 +0200

    chore: add support for tsgo again woho

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -57,7 +57,7 @@ return {
               },
             },
           },
-          tsgo = false,
+          -- tsgo = false,
           -- tsgo = {
           --   init_options = {
           --     preferences = {
